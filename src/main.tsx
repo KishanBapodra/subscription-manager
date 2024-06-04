997bf7fa17a404d0079b7324c32f533882637464
Kishan Bapodra <kishanbapodra2002@gmail.com>
2024-06-04 22:31:15 +0400

    SEO Update

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="w-screen">
-      <App />
-    </div>
+    <App />
   </React.StrictMode>
 );
